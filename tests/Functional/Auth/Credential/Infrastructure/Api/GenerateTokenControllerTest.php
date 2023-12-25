6a790cf0ee9f653881d7d12139660a5ed0b2ff0e
Jose Ma Alias <josemariaalias@ipglobal.es>
2023-12-25 11:07:16 +0100

    Feat: init genertae token success

---
+++ tests/Functional/Auth/Credential/Infrastructure/Api/GenerateTokenControllerTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\Auth\Credential\Infrastructure\Api;
+
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+final class GenerateTokenControllerTest extends WebTestCase
+{
+    private KernelBrowser $client;
+
+    protected function setUp(): void
+    {
+        $this->client = self::createClient();
+    }
+
+    /** @test */
+    public function itShouldReturnAccessTokenWithGrantTypeClientCredential(): void
+    {
+        // GIVEN
+
+        $credentials = [
+            'grantType'    => 'client_credentials',
+            'clientId'     => 'admin',
+            'clientSecret' => 'admin',
+        ];
+
+        // WHEN
+
+        $this->client->request(
+            'POST',
+            'api/auth/tokens',
+            [],
+            [],
+            [],
+            json_encode($credentials, JSON_THROW_ON_ERROR)
+        );
+
+        $response = $this->client->getResponse();
+
+        // THEN
+
+        self::assertResponseIsSuccessful();
+    }
+}
